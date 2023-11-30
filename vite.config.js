10a26c43c19a9b97a0b3b3682b6553071b71b6d9
nick thomas <nichoth@gmail.com>
2023-11-30 10:20:02 -0800

    rm unnecessary dependency

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,13 +13,6 @@ export default defineConfig({
         logOverride: { 'this-is-undefined-in-esm': 'silent' }
     },
     publicDir: '_public',
-    css: {
-        postcss: {
-            plugins: [
-                postcssNesting
-            ],
-        },
-    },
     server: {
         port: 8888,
         host: true,
